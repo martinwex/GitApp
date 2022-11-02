a90884bd69e323932ad6b081935b0c1c2f475c1c
Martin Wex <wex.tin-online.de>
2022-11-02 13:23:02 +0100

    Martin Wex's initial commit

--- app/src/main/java/com/martin_wex/gitapp/MainActivity.java
+++ app/src/main/java/com/martin_wex/gitapp/MainActivity.java
@@ -6,8 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //This is our important change_
-    //Ich teste einen Commit:9:46 Uhr
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
